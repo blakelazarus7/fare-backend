720ed7cf800216638cacce5ff1e0d6efac700c55
blakelazarus7 <blake@eatfare.com>
2025-06-24 23:47:06 -0700

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,4 +1,4 @@
-Correct hello.js file
+
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "https://www.eatfare.com");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
